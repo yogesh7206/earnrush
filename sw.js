9172f5f4ae24e5ce64092b5af82070a970bb65cc
yogesh <sandhyabaghel905@gmail.com>
2026-08-21 18:11:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,27 @@
 const CACHE_NAME = "earnrush-v1";
 
-self.addEventListener("install", event => {
+const FILES_TO_CACHE = [
+  "/earnrush/",
+  "/earnrush/index.html",
+  "/earnrush/manifest.json",
+  "/earnrush/icon-192.png",
+  "/earnrush/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request).catch(() => caches.match(event.request))
   );
